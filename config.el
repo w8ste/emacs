3aa71c7dc747f20faf95e66ab14a2a0625d5b692
Hannes <hannes10042004@icloud.com>
2025-05-04 19:33:41 +0200

    Update gitignore and add multiple cursors

--- config.el
+++ config.el
@@ -384,8 +384,12 @@
 
 (require 'org-tempo)
 
-(use-package pdf-tools
-  :defer t)
+(use-package multiple-cursors
+  :ensure t
+  :bind (("C-S-c C-S-c" . mc/edit-lines)
+         ("C->"         . mc/mark-next-like-this)
+         ("C-<"         . mc/mark-previous-like-this)
+         ("C-c C-<"     . mc/mark-all-like-this)))
 
 (use-package perspective
   :custom
